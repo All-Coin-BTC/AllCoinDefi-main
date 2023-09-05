34afcf86ecc1f608474419a5b3ac502b2902e67b
DylanCodesStuff <104543815+DylanCodesStuff@users.noreply.github.com>
2023-09-04 23:27:44 -0400

    Update Footer.jsx
    Updated import for faChevronRight to import from fortawesome/free-solid-svg-icons. Was throwing an error from free-brands-svg-icons

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTwitter, faFacebook, faInstagram, faSkype, faLinkedin, faChevronRight } from "@fortawesome/free-brands-svg-icons";
-
+import { faTwitter, faFacebook, faInstagram, faSkype, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
 const Footer = () => {
   return (
     <footer id="footer">
